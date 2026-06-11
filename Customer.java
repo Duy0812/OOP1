bc74a49d188451b89bde1a8b7f7d9e430744c54e
Duy0812 <kinhvolakinh@gmail.com>
2026-06-11 13:04:15 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,20 +1,38 @@
 package model.entity;
+
 import pattern.observer.IObserver;
 
 public class Customer implements IObserver {
-    private String customerId;
-    private String fullName;
+	private String customerId;
+	private String fullName;
+	private String email;
+	private String password;
 
-    public Customer(String customerId, String fullName) {
-        this.customerId = customerId;
-        this.fullName = fullName;
-    }
+	public Customer(String customerId, String fullName, String email, String password) {
+		this.customerId = customerId;
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+	}
 
-    // View đóng vai trò nhận thông báo
-    @Override
-    public void update(String message) {
-        System.out.println("\n[🔔 THÔNG BÁO TỚI " + fullName.toUpperCase() + "] " + message);
-    }
-    
-    public String getCustomerId() { return customerId; }
+	@Override
+	public void update(String message) {
+		System.out.println("\n[🔔 TIN NHẮN ĐẾN " + fullName.toUpperCase() + "] " + message);
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
