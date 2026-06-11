b7670c8a794a15a9c2a65384863f8fe0dafcf4d6
Duy0812 <kinhvolakinh@gmail.com>
2026-06-11 13:03:54 +0700

    Update CreditCardPayment.java

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -1,18 +1,18 @@
 package pattern.strategy;
 
-public class CreditCardPayment implements IPayment {
+public class CreditCard implements IPayment {
     private String cardNumber;
     private String cardHolder;
 
-    public CreditCardPayment(String cardNumber, String cardHolder) {
+    public CreditCard(String cardNumber, String cardHolder) {
         this.cardNumber = cardNumber;
         this.cardHolder = cardHolder;
     }
 
     @Override
     public boolean processPayment(double amount) {
-        System.out.println("Đang kết nối cổng thanh toán thẻ...");
-        System.out.println("Giao dịch thành công: " + amount + " VND qua Thẻ tín dụng (" + cardNumber + ")");
+        System.out.println("Đang kết nối cổng ngân hàng...");
+        System.out.println(">> Giao dịch THÀNH CÔNG: " + String.format("%,.0f", amount) + " VND qua Thẻ tín dụng (" + cardNumber + ")");
         return true;
     }
 }
