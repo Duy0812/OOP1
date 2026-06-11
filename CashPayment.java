dc410f92558283599a6a73ee70a2d145a8264eed
Duy0812 <kinhvolakinh@gmail.com>
2026-06-11 13:03:34 +0700

    Update CashPayment.java

--- CashPayment.java
+++ CashPayment.java
@@ -1,9 +1,9 @@
 package pattern.strategy;
 
 public class CashPayment implements IPayment {
-    @Override
-    public boolean processPayment(double amount) {
-        System.out.println("Đã nhận đủ tiền mặt: " + amount + " VND tại quầy Lễ tân.");
-        return true;
-    }
+	@Override
+	public boolean processPayment(double amount) {
+		System.out.println("Đã nhận đủ tiền mặt: " + amount + " VND tại quầy Lễ tân.");
+		return true;
+	}
 }
